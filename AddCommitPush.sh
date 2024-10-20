b05f34b3a477356812fd148a31ee99e484e173b0
ZainabMasoudi <L00179213@atu.ie>
2024-10-20 09:09:51 +0100

    Automating add process

--- AddCommitPush.sh
+++ AddCommitPush.sh
@@ -8,7 +8,20 @@
 clear
 git status
 
-echo '**************************************************'
-echo "Performing an add for all files in this directory"
+echo '*********************************'
+echo "add files in current repo"
 git add .
 git status
+
+echo '********************************'
+echo 'Enter the commit message:'
+read CommitMessage
+git commit -m "$CommitMessage"
+git status
+
+echo '******************************
+echo 'Pushing to GITHUB repository'
+git push -u origin main
+echo '*****************************'
+
+echo 'Done!'
